81ae5b99eb13a4af8c9de10226792c8287edda39
ramsey1 <35653163+ramsey1@users.noreply.github.com>
2018-06-09 12:02:48 +0530

    Add files via upload

---
+++ Day 1/PrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+package lecture3;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = (byte)19898;
+		short b = 20;
+		int c = 30;
+		long d = 40;
+		System.out.println(a);
+		if(true){
+			System.out.println("Inside if");
+		}
+		char ch = 'a';
+		System.out.println((int)ch);
+		
+		float x = 10.0F;
+		double y = 20.0;
+	}
+
+}
