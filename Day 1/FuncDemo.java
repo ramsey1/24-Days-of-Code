f62bb2ea2c4f76add15185881876e63c8cc47d04
ramsey1 <35653163+ramsey1@users.noreply.github.com>
2018-06-09 12:01:59 +0530

    Add files via upload

---
+++ Day 1/FuncDemo.java
@@ -0,0 +1,28 @@
+package lecture3;
+
+public class FuncDemo {
+	
+	public static int allAccess = 100;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+	
+		System.out.println(a+"-"+b);
+		swap(a, b);
+		System.out.println(a+"-"+b);
+		System.out.println(allAccess);
+	}
+	
+	public static int sum(int a, int b){
+		return a + b;
+	}
+	
+	public static void swap(int a, int b){
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+}
